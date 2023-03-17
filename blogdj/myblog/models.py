9a09d7734800c81914c1ffd0e3b6fb7b1e568f86
Daria M <milyakovad@gmail.com>
2023-03-17 21:46:03 +0300

    first commit

---
+++ blogdj/myblog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=200)
+    weight_g = models.IntegerField()
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+class Person(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.text
+
+
+
